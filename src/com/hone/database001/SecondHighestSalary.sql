5e62ffbea3f9d58a37ae12d3eac7cbc42fc4ea82
xiaAde <xiaxjian@163.com>
2018-07-18 23:56:11 +0800

    AC

---
+++ src/com/hone/database001/SecondHighestSalary.sql
@@ -0,0 +1,25 @@
+/*
+ 第二高的薪水：https://leetcode-cn.com/problems/second-highest-salary/description/
+*/
+
+SELECT max(Salary) as SecondHighestSalary from Employee where SALARY < (select max(SALARY) from Employee); 
+
+
+/*
+当然我们也可以稍稍扩展一下将整个创建数据库及创建表格的方式都完成
+*/
+
+CREATE DATABASE leetcode
+
+--创建Employee表
+Create table If Not Exists Employee (
+	Id int, 
+	Salary int
+	)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--插入数据
+insert into Employee (Id, Salary) values ('1', '100');
+insert into Employee (Id, Salary) values ('2', '200');
+insert into Employee (Id, Salary) values ('3', '300');
+
